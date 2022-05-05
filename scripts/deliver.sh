d1e0be54457b1ae40388d2e6192b0003a6a500c4
batch32022 <103128139+batch32022@users.noreply.github.com>
2022-05-05 08:01:01 +0530

    Delete scripts directory

--- scripts/deliver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NAME=`mvn help:evaluate -Dexpression=project.name | grep “"^|^\[]"`
-
-VERSION `mvn help:evaluate  Dexpression=project.version | grep "^|^\[]"
-
-java jar target/${NAME}-${VERSION}.jar
-
