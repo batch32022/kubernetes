8a52812e96047e73d40b65e2a90e85847964004c
batch32022 <103128139+batch32022@users.noreply.github.com>
2022-05-05 07:57:52 +0530

    Update deliver.sh

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,17 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -x
-mvn jar:jar  install:install help:evaluate -Dexpression=project.name
-set +x
-
-set -x
 NAME=`mvn help:evaluate -Dexpression=project.name | grep “"^|^\[]"`
-set +x
 
-set -x
-VERSION `mvn help:evaluate  Dexpression=project.version | grep "^|^\[]"`
-set +x
+VERSION `mvn help:evaluate  Dexpression=project.version | grep "^|^\[]"
 
-set -x
 java jar target/${NAME}-${VERSION}.jar
 
