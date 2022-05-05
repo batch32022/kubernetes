54d20345fc51cd574e91c96bfcf4d3a6f7498f56
batch32022 <rojabm43@gmail.com>
2022-05-05 07:54:53 +0530

    add

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -1,16 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -x
-mvn jar:jar  install:install help:evaluate -Dexpression=project.name
-set +x
+NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 
-set -x
-NAME=`mvn help:evaluate -Dexpression=project.name | grep “"^|^\[]"`
-set +x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 
-set -x
-VERSION `mvn help:evaluate  Dexpression=project.version | grep "^|^\[]"`
-set +x
-
-set -x
-java jar target/${NAME}-${VERSION}.jar
+java -jar target/${NAME}-${VERSION}.jar
